2e280bfb938a64fd9f08145823822560383776aa
Gregory Casamento <greg.casamento@gmail.com>
2025-04-08 16:25:27 -0400

    Initial Commit

---
+++ Classic/ClassicView.h
@@ -0,0 +1,12 @@
+//
+//  ClassicView.h
+//  Classic
+//
+//  Created by Gregory John Casamento on 4/8/25.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface ClassicView : ScreenSaverView
+
+@end
---
+++ Classic/ClassicView.m
@@ -0,0 +1,51 @@
+//
+//  ClassicView.m
+//  Classic
+//
+//  Created by Gregory John Casamento on 4/8/25.
+//
+
+#import "ClassicView.h"
+
+@implementation ClassicView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
